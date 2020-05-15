5a10d36c9e224c76ea3f4a8c873481a0ee81ebd0
Elano Garcez <elanogarcez@gmail.com>
2020-05-15 08:11:00 -0300

    better epic scrap logic

--- app/controllers/scraper_controller.rb
+++ app/controllers/scraper_controller.rb
@@ -22,38 +22,63 @@ class ScraperController < ApplicationController
   end
 
   def epic_free_to_get
-    browser_args = %w[--headless --no-sandbox --disable-dev-shm-usage --disable-gpu]
-    chrome_bin   = ENV["GOOGLE_CHROME_SHIM"]
-
-    browser = Watir::Browser.new(:chrome, driver_path: chrome_bin, args: browser_args)
-    browser.goto 'https://www.epicgames.com/store/pt-BR/free-games'
-    sleep 1
-
-    frees = []
-    browser.spans().each do |span|
-      frees << span if check_if_span_is_game(span.text)
-    end
-
-    @games = []
-    frees.each do |free|
-      element = free.parent.parent # changed 7-5-2020
-      element = free.parent.parent.parent
-      game_name   = element.span("data-testid": "offer-title-info-title").text
-      game_image  = element.imgs().first.src
-      game_link   = element.parent.parent.attributes[:href]
-      game_date_from = element.times().first.text
-      game_date_to = element.times().last.text
-
-      @games << [game_name, game_image, game_link, game_date_from, game_date_to]
-    end
+    browser   = prepare_browser_scraped_page
+    free_list = create_free_list(browser)
+    @games    = create_game_list(free_list)
 
     base_error if @games.empty?
   end
 
+  def prepare_browser_scraped_page
+    browser_args = %w[--headless --no-sandbox --disable-dev-shm-usage --disable-gpu]
+    chrome_bin   = ENV["GOOGLE_CHROME_SHIM"]
+    Watir.default_timeout = 5
+    browser = Watir::Browser.new(:chrome, driver_path: chrome_bin, args: browser_args)
+    browser.goto 'https://www.epicgames.com/store/pt-BR/free-games'
+    sleep 1
+    browser
+  end
+
+  def create_game_list(free_list)
+    games = []
+    free_list.each do |free|
+      begin
+        game = epic_dom_to_game(free)
+        games << game if game.present?
+      rescue => error
+        scraper_logger(error, self.class)
+      end
+    end
+    games
+  end
+
+  def scraper_logger(error, class_name)
+    Rails.logger.error "Error at #{class_name}, message: #{error}"
+  end
+
+  def create_free_list(browser)
+    free_list = []
+    # https://www.w3schools.com/cssref/css_selectors.asp
+    browser.divs(css: 'div[class^="CardGrid-card_"]').each do |game_dom|
+      free_list << game_dom
+    end
+    free_list
+  end
+
+  def epic_dom_to_game(dom)
+    game_name   = dom.span("data-testid": "offer-title-info-title").text
+    game_image  = dom.imgs().first.src
+    game_link   = dom.as.first.attributes[:href]
+    game_date_from = dom.times().first.text
+    game_date_to = dom.times().last.text
+
+    [game_name, game_image, game_link, game_date_from, game_date_to]
+  end
+
   def check_if_span_is_game(span_text)
-    return true if span_text.include?('GRÁTIS')
-    return true if span_text.include?('BREVE')
-    return true if span_text.include?('REQUER')
+    return true if span_text == 'GRÁTIS | Requer ADF'
+    return true if span_text == 'EM BREVE'
+    # return true if span_text == 'REQUER'
     # return true if span_text.include?('JOGO')
   end
 
